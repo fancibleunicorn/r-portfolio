e57bedefef49512b4892b3cbcaacd4c969e7d9bd
Adam Crandall <adamcrandall91@gmail.com>
2021-03-06 18:23:15 -0500

    pages render dynamically and clicking fancibleunicorn refreshes page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
@@ -7,15 +8,59 @@ import About from './components/About';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 
+
+
 function App() {
+
+const [contactSelected, setContactSelected] = useState(false);
+const [aboutSelected, setAboutSelected] = useState(false);
+const [projectSelected,setProjectSelected] = useState(false);
+const [resumeSelected, setResumeSelected] = useState(false);
+
   return (
     <body className="bg-light">
-      <Nav></Nav> 
-      <Main></Main>
-      <About></About>
-      <Projects></Projects>
-      <Resume></Resume>
-      <Contact></Contact>
+      <Nav
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      aboutSelected={aboutSelected}
+      setAboutSelected={setAboutSelected}
+      projectSelected={projectSelected}
+      setProjectSelected={setProjectSelected}
+      resumeSelected={resumeSelected}
+      setResumeSelected={setResumeSelected}
+
+      ></Nav> 
+
+      {!contactSelected && !aboutSelected && !projectSelected &&!resumeSelected ? (
+        <Main></Main>
+      ) : (
+        <div></div>
+      )}
+
+      {contactSelected ? (
+        <Contact></Contact>
+      ) : (
+      <div></div>
+      )}
+
+      {aboutSelected ? (
+        <About></About>
+      ) :
+      <div></div>
+      }
+
+      {projectSelected ? (
+        <Projects></Projects>
+      ) :
+      <div></div>
+      }
+
+      {resumeSelected ? (
+        <Resume></Resume>
+      ) :
+      <div></div>
+      }
+
       <Footer></Footer>
     </body>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,75 @@
 import React from 'react';
 
-function Nav() {
+function Nav(props) {
+    const {
+        setContactSelected,
+        setAboutSelected,
+        setProjectSelected,
+        setResumeSelected
+    } = props;
+
+    function contactClick() {
+        setContactSelected(true);
+        setAboutSelected(false);
+        setProjectSelected(false);
+        setResumeSelected(false);
+    }
+
+    function aboutClick() {
+        setContactSelected(false);
+        setAboutSelected(true);
+        setProjectSelected(false);
+        setResumeSelected(false);
+    }
+
+    function projectClick() {
+        setContactSelected(false);
+        setAboutSelected(false);
+        setProjectSelected(true);
+        setResumeSelected(false);
+    }
+
+    function resumeClick() {
+        setContactSelected(false);
+        setAboutSelected(false);
+        setProjectSelected(false);
+        setResumeSelected(true);
+    }
+
+
     return(
     <header className="card rounded-larger m-1">
-        <h1 className="card-title text-light p-2 px-4 font-weight-bold">
-        @fancibleunicorn
+        <h1 id="header" className="card-title text-light p-2 px-4 font-weight-bold">
+        <a href="/">@fancibleunicorn</a>
         </h1>
             <ul className="nav">
                 <li className="nav-item">
-                    <a className="nav-link text-light" href="#about-me">About</a>
+                    <a className="nav-link text-light" href="#header" 
+                    onClick={() => 
+                       aboutClick()
+                    }
+                    >About</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-light" href="#work">Projects</a>
+                    <a className="nav-link text-light" href="#header" 
+                    onClick={() => 
+                    projectClick()
+                    }
+                    >Projects</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-light" href="#work">Resume</a>
+                    <a className="nav-link text-light" href="#header" 
+                    onClick={() => 
+                   resumeClick()
+                    }
+                    >Resume</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-light" href="#contact-me">Contact</a>
+                    <a className="nav-link text-light" href="#header" 
+                    onClick={()=> 
+                    contactClick()
+                    }
+                    >Contact</a>
                 </li>
             </ul>
     </header>
